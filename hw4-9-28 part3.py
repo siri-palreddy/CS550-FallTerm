37095be057e6944e0475b47f7f535e00d2d2055f
siri-palreddy <34071747+siri-palreddy@users.noreply.github.com>
2017-12-02 18:26:33 -0500

    Delete hw4-9-28 part3.py

--- hw4-9-28 part3.py
+++
@@ -1,4 +0,0 @@
-##HW Part 3:
-## Explain the code's output.
-## Both f and g output to the fibonacci sequence, except, f starts with 1, while g starts with 0,
-## and f ends with 987, and g ends at 610. For clarification, I tried out this code.
